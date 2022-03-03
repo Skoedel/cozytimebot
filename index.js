a1ef70f310afe0c6eea05c1560ef7654c54a545e
Skoedel <90218510+discordbotuwu@users.noreply.github.com>
2022-03-03 22:43:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ if (message.content.toLowerCase().startsWith(prefix + "zeg"))
 }})
 
 
-
+if (message.content.startsWith(config.prefix + 'avatar')) {
+  const user = message.mentions.users.first() || message.author;
+  const avatarEmbed = new Discord.RichEmbed()
+      .setColor(0x333333)
+      .setAuthor(user.username)
+      .setImage(user.avatarURL);
+  message.channel.send(avatarEmbed);
+}
 
 
 client.on('message', message => {
