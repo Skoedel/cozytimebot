0b4987e34b400ca57697a199373dd85f6e28c0b0
Skoedel <90218510+discordbotuwu@users.noreply.github.com>
2022-03-03 21:45:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,14 +78,6 @@ client.on('message', message => {
   
 
   
-  
-  client.on('guildMemberRemove', guildMember => {
-    guildMember.guild.channels.cache.get('869522844609044510').send(`<@${guildMember.id}> left the server.` )
-  });
-  
-   client.on('guildMemberAdd', guildMember => {
-    guildMember.guild.channels.cache.get('869522844609044510').send(`<@${guildMember.id}> joined the server.` )
-  });
 
 
 
@@ -96,17 +88,14 @@ client.on('message', message => {
 client.on('message', message => {
  if(message.content.startsWith(prefix + "help", "commands")){
     const embed = new MessageEmbed()
-  .setTitle('Hey, here is a list of all the commands you can use!')
+  .setTitle('Hey, hier is een lijst van de commands die je kan gebruiken!')
   .setColor('#663702')
   .setTimestamp()
   .addFields(
     
 
-  { name: prefix+'grap', value: 'Om een grap te krijgen!' }, 
-  { name: prefix+'meme', value: 'Om een meme te krijgen!' }, 
   { name: prefix+'zeg', value: 'Om de bot een tekst te laten zeggen!' }, 
   { name: prefix+'vraag', value: 'Om een random vraag te krijgen!' }, 
-  { name: prefix+'nieuws', value: 'Om een (Engels) nieuws artikel te krijgen!' }, 
   { name: prefix+'serverinfo', value: 'Om de server informatie te krijgen!' },
    )
     ;
@@ -125,99 +114,10 @@ client.on('message', message => {
       });
 
 
-      client.on('message', message => {
-        if (message.content.toLowerCase().startsWith("123")) 
-         {
-          let MSG = message.content.split(" ");
-          let Query = MSG.slice(1).join("+");
-          let QueryD = MSG.slice(1).join(" ");
-          if (!Query) message.reply("Please specify something for me to say!")
-          else
-          {
-            message.channel.send(QueryD + " -" + "<@" + message.author.id + ">")
-           
-          message.react("1️⃣");
-          message.react('2️⃣');
-        }
-        }})
-
 
     
 
     
-
-
-
-    
-
-client.on('message', message => {
-  if(message.content.startsWith(prefix + "meme")){
-    redditFetch({
-      subreddit: 'memes', 
-      sort: 'top',
-      allowedNSFW: false,
-      allowedModPost: false,
-      allowCrossPost: true,
-      allowedVideo: true,
-    }).then(post => {
-        const embed = new MessageEmbed()
-          .setTitle('Hier is een meme voor je!')
-          .setURL(`${post.url}`)
-          .setColor('#663702')
-          .setFooter(`Deze meme is gepost in r/memes door u/${post.author}.`)
-          .setTimestamp()
-          .setImage(`${post.url}`);
-          
-          message.channel.send(embed);
-      });
-    }
-  });
-
-
-client.on('message', message => {
-if(message.content.startsWith(prefix + "grap")){
-  redditFetch({
-    subreddit: 'Jokes', 
-    sort: 'top',
-    allowedNSFW: false,
-    allowedModPost: false,
-    allowCrossPost: false,
-  }).then(post => {
-      const embed = new MessageEmbed()
-        .setTitle(`${post.title}`)
-        .setURL(`${post.url}`)
-        .setColor('#663702')
-        .setDescription(`||${post.selftext}||`)
-        .setFooter(`Deze grap is gepost in r/jokes door u/${post.author}.`)
-        .setTimestamp()
-        message.channel.send(embed);
-    });
-  }
-});
-
-
-const { Client, MessageEmbed } = require('discord.js')
-const redditFetch = require('reddit-fetch');
-client.on('message', message => {
-   if(message.content.startsWith(prefix + 'nieuws')){
-     redditFetch({
-       subreddit: 'news', 
-       sort: 'hot',
-       allowedNSFW: false,
-       allowedModPost: false,
-       allowCrossPost: false,
-     }).then(post => {
-         const embed = new MessageEmbed()
-           .setTitle(`${post.title}`)
-           .setURL(`${post.url}`)
-           .setColor('#663702')
-           .setTimestamp()
-           .setFooter(`Klik op de titel om het artikel te lezen.`)
-           message.channel.send(embed);
-       });
-     }
-   });
-
 
    client.on('message', message => {
     if(message.content.startsWith(prefix + "vraag")){
