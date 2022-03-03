b893ea72161df3016e8b6ac3732797d2bca00e16
Skoedel <90218510+discordbotuwu@users.noreply.github.com>
2022-03-03 14:07:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -116,21 +116,14 @@ client.on('message', message => {
 
 
      
+    client.on('message', message => {
+      if(message.content.startsWith("[POLL]")){
+      message.react(":one:");
+      message.react(':two:');
+               
+      }
+      });
 
-client.on('message', message => {
-  if (message.content.toLowerCase().startsWith("[POLL]")) 
-  {
-  let MSG = message.content.split(" ");
-  let Query = MSG.slice(1).join("+");
-  let QueryD = MSG.slice(1).join(" ");
-  if (!Query) message.reply("Please specify something for me to say!")
-  else
-  {
-    message.channel.send(QueryD + " -" + "<@" + message.author.id + ">")
-    message.react("1️⃣");
-    message.react('2️⃣');
-}
-}})
 
 
 
